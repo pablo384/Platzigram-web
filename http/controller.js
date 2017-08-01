3e36ab10ee5b6b6450f86d6247d92d52c0f05ec0
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-01 19:43:19 -0400

    peticiones http AJAX solo falta mostrar en el html

--- http/controller.js
+++ http/controller.js
@@ -1,4 +1,13 @@
 angular.module("MyFirstApp", [])
-.controller("FirstController", function ($scope) {
-	
+.controller("FirstController", function ($scope, $http) {
+	$http({
+      method: 'GET',
+      url: 'http://jsonplaceholder.typicode.com/posts'
+   }).then(function (data){
+   	console.log(data);
+   	$scope.posts=data;
+
+   },function (error){
+
+   });
 });
