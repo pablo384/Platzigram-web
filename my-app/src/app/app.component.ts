22a8d0c68f50d7eafda9a382e1c8a86d88244f04
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-03 22:13:32 -0400

    Two-way binding

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-export class Hero{	
-	id: number;
-	name:string;
+export class Hero{  
+    id: number;
+    name:string;
 }
 
 @Component({
@@ -12,13 +12,16 @@ export class Hero{
   template:`<h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
-  <div><label>Name: </label>{{hero.name}}</div>`
+  <div>
+  <label>name: </label>
+  <input [(ngModel)]="hero.name" placeholder="name">
+  </div>`
 })
 export class AppComponent {
   title = 'My First Angular App';
   hero : Hero = {
-  	id:1,
-  	name: 'Windstorm'
+    id:1,
+    name: 'Windstorm'
 
   };
 }
