4f14510e6a297c008c91eb96bdba86cb3447137d
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-03 22:01:22 -0400

    Hero object created

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Hero{	
+	id: number;
+	name:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:`<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
 export class AppComponent {
   title = 'My First Angular App';
+  hero : Hero = {
+  	id:1,
+  	name: 'Windstorm'
+
+  };
 }
+
