c2ccdbf15ea6b5caf14808557b55bf3f61280000
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-06 19:02:06 -0400

    Routing -Splitting the AppComponent --HeroesComponent --Create AppComponent -Add routing --<base href> --Configure routes --Make the router available --Router outlet --Router links -Add a dashboard --Configure the dashboard route --Add a redirect route --Add navigation to the template -Add heroes to the dashboard --Sharing the HeroService --Get heroes -Navigating to hero details --Routing to a hero detail --Parameterized route --Configure a route with a parameter -Revise the HeroDetailComponent --Add HeroService.getHero() --Find the way back -Select a dashboard hero -Refactor routes to a Routing Module --Update AppModule

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,31 +1,17 @@
 import { Component } from '@angular/core';
-import {Hero} from './hero';
-import {HeroService} from './hero.service';
-import {OnInit} from '@angular/core';
-
-
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  // styleUrls: ['./heroes.component.css'],
+  // template: `
+  //   <h1>{{title}}</h1>
+  //   <a routerLink="/heroes">Heroes</a>
+  //   <router-outlet></router-outlet>
+  // `
   
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  constructor(private heroService:HeroService){}
-  selectedHero: Hero;
-  onSelect(hero:Hero): void{
-      this.selectedHero=hero;
-  }
-  getHeroes():void{
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  ngOnInit():void{
-    this.getHeroes();
-  }
   
 }
-
--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -7,5 +7,9 @@ export class HeroService{
 	getHeroes():Promise<Hero[]>{
 		return Promise.resolve(HEROES);
 	}
+	getHero(id: Number):Promise<Hero>{
+		return this.getHeroes()
+		.then(heroes => heroes.find(hero => hero.id === id));
+	}
 
 }
