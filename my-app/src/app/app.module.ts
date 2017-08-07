8820f5666e3360b109333352dd6892266a7b0cab
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-07 06:48:37 -0400

    Providing HTTP Services -Register for HTTP services -Simulate the web API -npm install angular-in-memory-web-api --save -Heroes and HTTP

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
+
+import {AppRoutingModule} from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 import {HeroDetailComponent} from './hero-detail.component';
 import { AppComponent } from './app.component';
@@ -9,7 +17,7 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
-import {AppRoutingModule} from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +29,8 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
