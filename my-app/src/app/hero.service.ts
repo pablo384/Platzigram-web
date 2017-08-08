0057ea312d4461c98f69ad0dcde8d864b6b95ea2
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-08 18:50:48 -0400

    Heroes and HTTP -HTTP Promise -Extracting the data in the then callback -Error Handling -Get hero by id -Unchanged getHeroes API Updating hero details -Add the ability to save hero details -Add a hero service update() method Add the ability to add heroes Add the ability to delete a hero -Hero service delete() method

--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -11,6 +11,7 @@ import {HEROES} from './mock-heroes';
 export class HeroService{
 
 	private heroesUrl = 'api/heroes';  // URL to web api
+	private headers = new Headers({'Content-Type': 'application/json'});
 
 	constructor(private http: Http) { }
 
@@ -23,6 +24,7 @@ export class HeroService{
 
 	private handleError(error: any): Promise<any> {
 		console.error('An error occurred', error); // for demo purposes only
+		console.log(error);
 		return Promise.reject(error.message || error);
 	}
 
@@ -30,8 +32,37 @@ export class HeroService{
 		// 	return Promise.resolve(HEROES);
 		// }
 		getHero(id: Number):Promise<Hero>{
-			return this.getHeroes()
-			.then(heroes => heroes.find(hero => hero.id === id));
+			const url = `${this.heroesUrl}/${id}`;
+			return this.http.get(url)
+			.toPromise()
+			.then(response => response.json().data as Hero)
+			.catch(this.handleError);
+		}
+		
+
+		update(hero: Hero): Promise<Hero> {
+			const url = `${this.heroesUrl}/${hero.id}`;
+			return this.http
+			.put(url, JSON.stringify(hero), {headers: this.headers})
+			.toPromise()
+			.then(() => hero)
+			.catch(this.handleError);
+		}
+
+		create(name: string): Promise<Hero> {
+			return this.http
+			.post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+			.toPromise()
+			.then(res => res.json().data as Hero)
+			.catch(this.handleError);
+		}
+		delete(id: number): Promise<void> {
+			const url = `${this.heroesUrl}/${id}`;
+			return this.http.delete(url, {headers: this.headers})
+			.toPromise()
+			.then(() => null)
+			.catch(this.handleError);
 		}
 
 	}
+	
